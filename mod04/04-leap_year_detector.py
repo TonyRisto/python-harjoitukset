8265b4b3e74123b9d7f53c6fd31023a6165c8d2d
Tony Risto <tony.risto@vennify.fi>
2026-08-25 15:19:38 +0300

    Module 04 task 4 done.

--- mod04/04-leap_year_detector.py
+++ mod04/04-leap_year_detector.py
@@ -1,6 +1,8 @@
 year = int(input("Anna vuosiluku (esim. 2020) "))
 
-if year % 4:
-    print(f"{year} ei ole karkausvuosi")
-else:
+if year % 400 == 0:
     print(f"{year} on karkausvuosi")
+elif (year % 4 == 0) and (year % 100 != 0):
+    print(f"{year} on karkausvuosi")
+else:
+    print(f"{year} ei ole karkausvuosi")
