cb8b68610ded0ced399e16b243d050d1e99d28ee
Tony Risto <tony.risto@vennify.fi>
2026-08-27 13:18:34 +0300

    Task 1 and 2 done for the game project

--- peliprojekti/README.md
+++ peliprojekti/README.md
@@ -1,3 +1,9 @@
 ## Peli
 
 # Tony Risto
+
+# Pelaajan nimi ja ikä kysytään.
+
+# Pelaajan ikä tarkistetaan (onko alle 12-vuotias)
+
+# Valikko kysyy komentoa ja jokainen valinta antaa erin tulosteen ja kysyy uuden komennon jonka jälkeen palaa päävalikkoon.
--- peliprojekti/game.py
+++ peliprojekti/game.py
@@ -1,4 +1,27 @@
 name = input("Mikä on nimesi? ")
 age = int(input("Mikä on ikäsi? "))
 
-print(f"Hei, {name}!\nOlet {age}-vuotias")
+if age < 12:
+    print(f"Kokeile {12 - age} vuoden päästä uudestaan :)")
+    quit()
+
+print(f"Hei, {age}-vuotias {name}!\nTervetuloa pelaamaan!")
+
+
+while True:
+    command = int(input("Valitse seuraavista komennoista:\n1. Aloita peli\n2. Ohjeet\n3. Tulostaulu\n4. Lopeta\n"))
+    state = command
+    while state != 4:
+        if state == 1:
+            state = input("Paina x näppäintä\n")
+        if state == 2:
+            state = input("Paina y näppäintä\n")
+        if state == 3:
+            state = input("Paina a näppäintä\n")
+        if state == "x" or state == "y" or state == "a":
+            command = int(input("Päävalikko: 1. Aloita peli, 2. Ohjeet, 3. Tulostaulu, 4. Lopeta\n"))
+            state = command
+        else:
+            print("Väärä näppäin, aloita alusta.")
+            break
+        
