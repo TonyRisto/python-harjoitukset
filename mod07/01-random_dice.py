3fed387430e6b3bc68f1ad75f830bb52736b8029
Tony Risto <tony.risto@vennify.fi>
2026-08-27 16:40:20 +0300

    Module 07 started, 1st task done

---
+++ mod07/01-random_dice.py
@@ -0,0 +1,12 @@
+import random
+
+def throw_dice():
+    dice = random.randint(1, 6)
+    return dice
+
+dice = throw_dice()
+print(dice)
+
+while dice != 6:
+    dice = throw_dice()
+    print(dice)
